4d88b5c461fbce23f3799dc16e62e134de24b848
riviere914 <72252251+riviere914@users.noreply.github.com>
2020-11-03 22:48:00 +0100

    ajout

---
+++ assets/js/main5.js
@@ -0,0 +1,9 @@
+function divideThem() {
+
+    var numberOne = document.getElementById('numberOne').value;
+    var numberTwo = document.getElementById('numberTwo').value;
+    var result= (numberOne % numberTwo);
+    alert (result);
+
+}
+
---
+++ assets/js/main6.js
@@ -0,0 +1,9 @@
+function calculThem() {
+
+    var numberOne = document.getElementById('numberOne').value;
+    var numberTwo = document.getElementById('numberTwo').value;
+    var result = ((((( numberOne * 2) + 5) * 50) - numberTwo) + 1769) ;
+    
+    alert ('le resultat est:'+ result);
+}
+
